6abaf9121ec39ec95af7b6366cf072ac9c51ed20
LenWinkler <lenwinkler86@gmail.com>
2019-10-04 11:53:28 -0400

    added playerCard

---
+++ client/src/components/playerCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const PlayerCard = (props) => {
+    return (
+        <div>
+            <p>{`Player name: ${props.name}`}</p>
+            <p>{`Country: ${props.country}`}</p>
+            <p>{`# of Searches: ${props.searches}`}</p>
+        </div>
+    )
+}
+
+export default PlayerCard;
