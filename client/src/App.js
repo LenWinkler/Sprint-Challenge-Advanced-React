a06177e584c930ca84cc322edfd9d80fe0a9916d
LenWinkler <lenwinkler86@gmail.com>
2019-10-04 12:17:30 -0400

    displaying information from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import axios from "axios";
+import PlayerCard from "./components/playerCard";
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+
+    }
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/api/players')
       .then(res => {
-        console.log(res.data);
+        this.setState(res.data[0]);
       })
+  }
+  
+  currentPlayer = () => {
+    
   }
   
 
@@ -20,7 +27,8 @@ class App extends React.Component {
     return (
       <div className="App">
         
-      <p>my app</p>
+      <p>Women's World Cup Player's Ranked by Search Interest(June-July 2019)</p>
+      <PlayerCard player={this.state}/>
 
       </div>
     );
--- client/src/components/playerCard.js
+++ client/src/components/playerCard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
+const CardDiv = styled.div`
+    width: 30%;
+    border: 1px solid black;
+`
+
 const PlayerCard = (props) => {
+    console.log(props)
     return (
-        <div>
-            <p>{`Player name: ${props.name}`}</p>
-            <p>{`Country: ${props.country}`}</p>
-            <p>{`# of Searches: ${props.searches}`}</p>
-        </div>
+        <CardDiv>
+            <p>{`Player name: ${props.player.name}`}</p>
+            <p>{`Country: ${props.player.country}`}</p>
+            <p>{`# of Searches: ${props.player.searches}`}</p>
+        </CardDiv>
     )
 }
 
